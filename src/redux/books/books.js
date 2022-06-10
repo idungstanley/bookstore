2e466148703b624afaaa70c5fe6f57d7ae562871
sunday stanley Idung <sundaystanley56@gmail.com>
2022-06-10 05:49:49 +0100

    Add major functionalities and redux store

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,15 +1,17 @@
 // Actions
 const ADDBOOK = 'books/reducerBook/ADDBOOK';
 const REMOVEBOOK = 'books/reducerBook/DELETE';
-const books = [];
+const books = [
+  { title: 'stan go strong', author: 'sunday', id: '1' },
+  { title: 'How to be rich', author: 'sunday', id: '2' },
+  { title: 'How successfuk people think', author: 'sunday', id: '3' },
+];
 
 // Reducers
 export default function reducerBook(state = books, action = {}) {
   switch (action.type) {
     case ADDBOOK:
-      return [
-        ...state, action.book,
-      ];
+      return [...state, action.book];
     case REMOVEBOOK:
       return state.filter((book) => book.id !== action.id);
     default:
